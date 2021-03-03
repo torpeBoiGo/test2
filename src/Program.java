3ec0f61aefbdf9f02741be614f33fbe2a82d750d
Bence Orosz <oroszbence0929@gmail.com>
2021-03-03 22:05:11 +0100

    het2 done

--- src/Program.java
+++ src/Program.java
@@ -1,6 +1,10 @@
 public class Program {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+<<<<<<< HEAD
         System.out.println("het2 done");
+        System.out.println("Marci kitalált, hgoy basszunk be!");
+=======
+>>>>>>> parent of b034e19... het2 done
     }
 }
