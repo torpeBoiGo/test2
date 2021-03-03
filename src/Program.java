c5e69a1efd1fa1c731e7ee0f363a17d0fb7a0f80
Bence Orosz <oroszbence0929@gmail.com>
2021-03-03 22:03:14 +0100

    goldi új ötlete

--- src/Program.java
+++ src/Program.java
@@ -2,5 +2,6 @@ public class Program {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("het2 done");
+        System.out.println("Goldi kitalálta, hogy lófasz van!");
     }
 }
