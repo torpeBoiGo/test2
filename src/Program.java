9097cd868e7a5f5bbd1eaa7a16fc6324cdfde1be
Bence Orosz <35062978+woranhun@users.noreply.github.com>
2021-03-03 22:23:37 +0100

    Het2 (#3)
    * het2 done
    
    * het2 done
    
    * goldi új ötlete (#1)
    
    * Marci azt mondta basszunk be (#2)

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,8 @@
 public class Program {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("het2 done");
+        System.out.println("Marci azt modnta basszunk be!");
+        System.out.println("Goldi kitalálta, hogy lófasz van!");
     }
 }
